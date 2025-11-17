2d2f513b654f8f26e2012a7cd5acfe88a765f252
dyonxsz <dyon.emanoell@gmail.com>
2025-11-16 21:43:51 -0300

    mod

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Login from "./login/page"
+
+export default function Home() {
+    return (
+        <div className="App">
+            <Login />
+          
+      
+        </div>
+    )
+}
